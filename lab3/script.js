28d6aa1acb99e1f776e56acd02cee122b5a03a26
baygu10ve1 <92426719+baygu10ve1@users.noreply.github.com>
2024-11-29 21:36:35 +0300

    7

--- lab3/script.js
+++ lab3/script.js
@@ -1,30 +1,28 @@
 document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Отменяет стандартное поведение формы
-
+    event.preventDefault(); 
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const username = document.getElementById("username").value;
     const age = parseInt(document.getElementById("age").value);
     const errorMessages = [];
 
-    // Валидация электронной почты
+
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailPattern.test(email)) {
         errorMessages.push("Пожалуйста, введите действующую электронную почту.");
     }
 
-    // Валидация номера телефона
     const phonePattern = /^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/;
     if (!phonePattern.test(phone)) {
         errorMessages.push("Пожалуйста, введите действительный номер телефона в формате +7 (___) ___-__-__.");
     }
 
-    // Валидация возраста
+   
     if (age < 18 || age > 90) {
         errorMessages.push("Возраст должен быть в диапазоне от 18 до 90 лет.");
     }
 
-    // Проверка на наличие ошибок и вывод их на экран
+    
     const errorMessagesDiv = document.getElementById("errorMessages");
     errorMessagesDiv.innerHTML = "";
 
@@ -35,7 +33,7 @@ document.getElementById("registrationForm").addEventListener("submit", function(
             errorMessagesDiv.appendChild(p);
         });
     } else {
-        alert('Регистрация успешна!'); // Здесь можно продолжить работу с формой (например, отправить на сервер)
-        document.getElementById("registrationForm").reset(); // Сбросить форму после успешной регистрации
+        alert('Регистрация успешна!'); 
+        document.getElementById("registrationForm").reset(); 
     }
 });
